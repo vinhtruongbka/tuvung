5629897e8e34b63a93d331646062a221a5ca0a6f
Your Name <you@example.com>
2019-02-16 19:39:18 +0700

    make it better

---
+++ database/migrations/2019_02_16_123838_create_news_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idCategory');
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('desc');
+            $table->string('images', 100);
+            $table->text('content');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
