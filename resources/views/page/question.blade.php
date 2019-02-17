badd7648b9b75a89cd99b797be28824b92a4bf4e
Your Name <you@example.com>
2019-02-17 21:25:31 +0700

    make it better

--- resources/views/page/question.blade.php
+++ resources/views/page/question.blade.php
@@ -7,24 +7,30 @@
           <div class="klpt">
             <div class="title_show_all">
               <div class="title_show_list_h1"><h1>danh sách từ vựng tiếng hàn xuất khẩu lao động</h1></div>
-            </div>  
-            <ul class="listz_two"> 
-             @php
-               $flag = 0;
-             @endphp
-              @foreach ($desc as $des)
-              @php
-                 $flag = $flag+1;
-               @endphp 
-              <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 padding">
-                <li>
-                  <a href="{{ route('home.LearningWords',['slug1' => $des->categorySlug, 'slug2' => $des->categorychiSlug])}}">@php
-                    echo $flag;
-                  @endphp. {{$des->categorychiTitle}}</a>
-                </li>
-              </div>
-              @endforeach
-            </ul>
+            </div> 
+            @if (!$desc ->isEmpty())
+               <ul class="listz_two"> 
+                 @php
+                   $flag = 0;
+                 @endphp
+                  @foreach ($desc as $des)
+                  @php
+                     $flag = $flag+1;
+                   @endphp 
+                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 padding">
+                    <li>
+                      <a href="{{ route('home.LearningWords',['slug1' => $des->categorySlug, 'slug2' => $des->categorychiSlug])}}">@php
+                        echo $flag;
+                      @endphp. {{$des->categorychiTitle}}</a>
+                    </li>
+                  </div>
+                  @endforeach
+               </ul>
+             @else
+               <div class="alert alert-danger" style="margin-top: 20px;">
+                    Hiện tại chưa có danh sách từ vựng nào!
+                 </div>
+             @endif 
           </div> 
         </div>
       </div>
