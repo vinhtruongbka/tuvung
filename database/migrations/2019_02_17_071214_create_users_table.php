84cb0e983d53d4a89a9e379b023e6300224501fb
Your Name <you@example.com>
2019-02-17 14:15:46 +0700

    make it better

--- database/migrations/2019_02_17_071214_create_users_table.php
+++ database/migrations/2019_02_17_071214_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('money');
+            $table->integer('money')->default(0);
             $table->date('endDate')->nullable();;
             $table->string('sex');
             $table->string('images')->nullable();;
