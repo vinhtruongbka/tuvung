64ff4970503893c6e6e5d870e84eb31699fed7e4
Your Name <you@example.com>
2019-02-20 00:55:38 +0700

    make it better

--- app/Helper/AppHelper.php
+++ app/Helper/AppHelper.php
@@ -35,16 +35,12 @@ function randomVietnam($answer,$arrayAnswer,$keyString){
 	}
 
    function get_user_ip() {
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') > 0) {
-                $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
-                return trim($addr[0]);
-            } else {
-                return $_SERVER['HTTP_X_FORWARDED_FOR'];
-            }
+        if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
-            return $_SERVER['REMOTE_ADDR'];
+            $ip_address = $_SERVER['REMOTE_ADDR'];
         }
+        return $ip_address;
     }
 
   function countView()
