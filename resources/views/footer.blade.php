99afcb93c578e9cf1a5289c2e4f44ce80605536b
Your Name <vinhtruongbk@gmail.com>
2019-02-22 10:48:54 +0700

    make it better

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,39 @@
+<style type="text/css">
+	.mynav{
+		margin-bottom: 15px;
+	}
+	.mynav li{
+		padding-left: 10px !important;
+	}
+	.mynav li a{
+		color: #fff !important;
+		padding: 0px !important;
+	}
+	.mynav li a:hover{
+		background-color: #2F4F4F !important;
+	}
+</style>
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 padding">
+	<ul class="nav nav-pills mynav">
+        @if (Auth::check())
+            <li>
+                <a href='{{ route('home.getMoney')}}'>Quản trị</a>
+            </li>
+            <li>
+                <a href='{{ route('postLogout')}}'>Đăng xuất</a>
+            </li>
+        @else
+            <li>
+                <a href='{{ route('home.getRegisration')}}'>Đăng ký</a>
+            </li>
+            <li>
+                <a href='{{ route('getLogin')}}'>Đăng nhập</a>
+            </li>
+        @endif 
+        <li>
+              <a href='{{ route('getRecharge')}}'>Nạp tiền</a>
+        </li>
+    </ul>
 	{!! $address->content !!}
 
  </div>
